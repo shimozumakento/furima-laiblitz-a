8056f06259187d6e8c5fab2ca381a75fc079f65c
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-16 17:57:54 +0900

    Active Hash変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :condition
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :shipping_cost
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :shipping_area
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :shipping_day
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
