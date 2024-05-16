eb5026fdae928b999ff13b0c5ac26e1d7bcbc670
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-16 15:40:34 +0900

    controller名の変更
    items→products

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
+<<<<<<<< HEAD:test/controllers/users_controller_test.rb
 class UsersControllerTest < ActionDispatch::IntegrationTest
+========
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+>>>>>>>> 89217c0 (controller名の変更):test/controllers/products_controller_test.rb
   # test "the truth" do
   #   assert true
   # end
