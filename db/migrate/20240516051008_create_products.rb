cc5a60be3ff330f719ef8841323127195d066f73
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-16 14:13:51 +0900

    productモデルの作成からmigrate

---
+++ db/migrate/20240516051008_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :product_name, null: false
+      t.text :description, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :shipping_cost_id, null: false
+      t.integer :shipping_area_id, null: false
+      t.integer :shipping_day_id, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
