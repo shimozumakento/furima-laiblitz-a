c16b1d69aa45287c47e0d51fdaadec4ac75d23ab
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-16 15:51:20 +0900

    出品のため、controllerの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,18 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @Product=Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:user, :product_name, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price)
+  end
+  
 end
