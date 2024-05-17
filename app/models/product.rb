0e9e6ed1c2203e40aa0f0efdd7da75cfdb6677ca
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-17 16:30:11 +0900

    エラーメッセージの表示設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, presence: true
+  validates :product_name, presence: true
+  validates :description, presence: true
   belongs_to :category
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
@@ -14,4 +17,8 @@ class Product < ApplicationRecord
 
   belongs_to :shipping_day
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999  }
+
+  has_one_attached :image
 end
