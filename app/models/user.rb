68aaef62df69c710f88840c46aae2b5d3b3b0589
SeiyaShimizu <167734954+SeiyaShimizu@users.noreply.github.com>
2024-05-17 13:33:17 +0900

    エラーメッセージの編集とログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字混合で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/, message: '全角カタカナを使用してください' }
-  validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :birthday, presence: true
 
 end
