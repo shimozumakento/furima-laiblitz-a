09e64eb061983b6f64036f87f20adf282237da6e
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-20 12:32:27 +0900

    ログイン制限機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only:[:new,:create]
   def index
   end
 
@@ -8,6 +9,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user_id = current_user.id
     if @product.save
       redirect_to root_path
     else
@@ -17,7 +19,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:user, :product_name, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price, :image)
+    params.require(:product).permit(:user_id, :product_name, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price, :image)
   end
   
 end
