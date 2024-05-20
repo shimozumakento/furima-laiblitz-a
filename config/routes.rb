51fdd03c25ef32d7a986c86061d7ee26d158111f
SeiyaShimizu <167734954+SeiyaShimizu@users.noreply.github.com>
2024-05-20 13:22:50 +0900

    productsテーブル内データによる条件分岐

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root "products#index"
-  resources :products,only: [:index] 
+  resources :products
 end
