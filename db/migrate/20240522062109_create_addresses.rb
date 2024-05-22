cec039d88b229f6b458937cde0881570465d0ea0
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-05-22 15:52:07 +0900

    カラム名の変更

--- db/migrate/20240522062109_create_addresses.rb
+++ db/migrate/20240522062109_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :history, null: false, foreign_key: true
       t.string :postal_code, null: false
       t.integer :shipping_area_id, null: false
       t.string :city, null: false
